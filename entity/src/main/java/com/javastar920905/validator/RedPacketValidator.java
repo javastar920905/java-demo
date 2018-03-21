fc6ba72e2398672b8f41e112f877b8a2b3e8f806
ouzhx <ouzhx@lt51.cn>
2018-03-21 11:20:08 +0800

    web层添加validator 校验器

---
+++ entity/src/main/java/com/javastar920905/validator/RedPacketValidator.java
@@ -0,0 +1,99 @@
+package com.javastar920905.validator;
+
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.javastar920905.entity.domain.RedPacket;
+import org.springframework.beans.propertyeditors.ByteArrayPropertyEditor;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.SmartValidator;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+/**
+ * 红包属性校验器
+ * 
+ * @author ouzhx on 2018/3/21.
+ */
+public class RedPacketValidator implements Validator {
+
+  /*
+   * private final SmartValidator smartValidator;
+   * 
+   *//**
+      * 构造器方式组合其他校验器
+      * 
+      * @param smartValidator
+      *//*
+         * public RedPacketValidator(SmartValidator smartValidator) { if (smartValidator == null) {
+         * throw new IllegalArgumentException( "The supplied [Validator] is " +
+         * "required and must not be null."); } this.smartValidator = smartValidator; }
+         */
+
+  /**
+   * 验证当前实例是否属于当前clazz Can this {@link Validator} {@link #validate(Object, Errors) validate}
+   * instances of the supplied {@code clazz}?
+   * <p>
+   * This method is <i>typically</i> implemented like so:
+   * 
+   * <pre class="code">
+   * return Foo.class.isAssignableFrom(clazz);
+   * </pre>
+   * 
+   * (Where {@code Foo} is the class (or superclass) of the actual object instance that is to be
+   * {@link #validate(Object, Errors) validated}.)
+   *
+   * @param clazz the {@link Class} that this {@link Validator} is being asked if it can
+   *        {@link #validate(Object, Errors) validate}
+   * @return {@code true} if this {@link Validator} can indeed {@link #validate(Object, Errors)
+   *         validate} instances of the supplied {@code clazz}
+   */
+  @Override
+  public boolean supports(Class<?> clazz) {
+    return RedPacket.class.equals(clazz);
+  }
+
+  /**
+   * object: 待校验的对象; errors: 错误报告信息 Validate the supplied {@code target} object, which must be of a
+   * {@link Class} for which the {@link #supports(Class)} method typically has (or would) return
+   * {@code true}.
+   * <p>
+   * The supplied {@link Errors errors} instance can be used to report any resulting validation
+   * errors.
+   *
+   * @param target the object that is to be validated (can be {@code null})
+   * @param errors contextual state about the validation process (never {@code null})
+   * @see ValidationUtils
+   */
+  @Override
+  public void validate(Object target, Errors errors) {
+    // 当null 或 空字符串时拒绝
+    ValidationUtils.rejectIfEmpty(errors, "userId", "userId 不能为空");
+    ValidationUtils.rejectIfEmpty(errors, "money", "oepnId 不能为空");
+    ValidationUtils.rejectIfEmpty(errors, "packetSize", "红包数量 不能为空");
+    if (errors.hasErrors()) {
+      for (FieldError fieldError : errors.getFieldErrors()) {
+        System.out.println(fieldError.getField());
+        System.out.println(fieldError.getRejectedValue());
+        // ...
+      }
+      return;
+    }
+    RedPacket redPacket = (RedPacket) target;
+    // 每个属性都需要做为空判断,验证器只会把错误信息填充到errors对象, 不会中断后面的语句
+    if (redPacket.getPacketSize() != null) {
+      if (redPacket.getPacketSize() < 1) {
+        errors.rejectValue("packetSize", "红包数量不能少于1个");
+      }
+    }
+    if (redPacket.getMoney() != null) {
+      if (redPacket.getMoney() > 200) {
+        errors.rejectValue("packetSize", "红包金额不能大于200");
+      }
+      if (redPacket.getMoney() < (0.01 * redPacket.getPacketSize())) {
+        errors.rejectValue("packetSize", "每个红包金额不能少于0.01");
+      }
+    }
+
+
+  }
+}
--- web/src/main/java/com/javastar920905/web/controller/Index.java
+++ web/src/main/java/com/javastar920905/web/controller/Index.java
@@ -1,10 +1,28 @@
 package com.javastar920905.web.controller;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.javastar920905.entity.domain.RedPacket;
+import com.javastar920905.service.pay.IRedPacketService;
+import com.javastar920905.validator.RedPacketValidator;
+import jodd.util.MimeTypes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @author ouzhx on 2018/3/6.
@@ -12,6 +30,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class Index {
+  /*
+   * @Autowired private IRedPacketService redPacketService;
+   */
+
+  @InitBinder
+  protected void initBinder(WebDataBinder binder) {
+    binder.setValidator(new RedPacketValidator());
+  }
+
   @GetMapping("/")
   public String hello(HttpSession session) {
     session.setAttribute("web", "web project attribute");
@@ -21,4 +48,38 @@ public class Index {
     }
     return "hello " + web2Attr;
   }
+
+  /**
+   * 发红包 测试:通过
+   *
+   * http://localhost:8080/giveRedPacket?userId=1&money=2&packetSize=2
+   * 
+   * @param redPacket
+   * @param bindingResult
+   * @return produces = MimeTypes.MIME_APPLICATION_JSON, consumes = MimeTypes.MIME_APPLICATION_JSON
+   */
+  @GetMapping(value = "/giveRedPacket")
+  public String giveRedPacket(@Validated @ModelAttribute RedPacket redPacket,
+      @RequestParam Map<String, String> allRequestParams, BindingResult bindingResult) {
+
+    // 请求转换为对象技巧
+    JSONObject redPacketJson = JSONObject
+        .parseObject(JSON.toJSONString(allRequestParams, SerializerFeature.WriteMapNullValue));
+
+    // 数据校验处理
+    if (bindingResult.hasErrors()) {
+      return "fail";
+    }
+    try {
+      /*
+       * JSONObject jsonObject = redPacketService.giveRedPacket(redPacket); return
+       * jsonObject.toJSONString();
+       */
+      return "success";
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return "fail";
+  }
+
 }
