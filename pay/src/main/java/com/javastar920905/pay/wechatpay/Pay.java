98ee12250fb8668031d07cac3a7e8fd1efb058a6
ouzhx <ouzhx@lt51.cn>
2018-03-26 11:27:47 +0800

    添加微信支付文档说明

---
+++ pay/src/main/java/com/javastar920905/pay/wechatpay/Pay.java
@@ -0,0 +1,13 @@
+package com.javastar920905.pay.wechatpay;
+
+/**
+ * @author ouzhx on 2018/3/26.
+ *
+ * 微信小程序统一下单api
+ *
+ * 小程序支付接口文档 https://mp.weixin.qq.com/debug/wxadoc/dev/api/api-pay.html#wxrequestpaymentobject
+ *
+ * 接口调用时序图 https://pay.weixin.qq.com/wiki/doc/api/wxa/wxa_api.php?chapter=7_4&index=3
+ */
+public class Pay {
+}
