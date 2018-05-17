992d8298a72439db09207b8f77f976c5579eac86
ouzhx <ouzhx@lt51.cn>
2018-05-17 13:51:16 +0800

    完善jsonutil

--- tools/src/main/java/com/javastar920905/outer/JSONUtil.java
+++ tools/src/main/java/com/javastar920905/outer/JSONUtil.java
@@ -6,12 +6,16 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.PropertyPreFilter;
 import com.alibaba.fastjson.serializer.SerializeFilter;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
 
 import java.util.Set;
 
 /**
  * Created by ouzhx on 2016/12/2.
+ *
+ * 参考文档:
+ * http://note.youdao.com/noteshare?id=abf2b149d54fe631590212f6603b3220&sub=99E4BE4198BC42DEA0681470A5587B5E
  */
 public class JSONUtil {
 
@@ -59,12 +63,39 @@ public class JSONUtil {
    * 把Java对象转换成JSON对象
    * 
    * @param object
-   * @param filter
+   * @param filter 属性过滤器 如: getIncludeFilter("value","name") 只包含value 和name 属性
    * @return JSONObject
    * @author chenjun
    */
   public static JSONObject parseObjectToJSONObject(Object object, SerializeFilter filter) {
     String jsonStr = JSON.toJSONString(object, filter);
+
+    return JSON.parseObject(jsonStr);
+  }
+
+  /**
+   * 把Java对象转换成JSON对象(带时间,包含值为null的字段)
+   *
+   * @param object
+   * @return JSONObject
+   * @author chenjun
+   */
+  public static JSONObject parseObjectToJSONObject(Object object) {
+    String jsonStr = JSON.toJSONStringWithDateFormat(object, "yyyy-MM-dd hh:mm:ss",
+        SerializerFeature.WriteMapNullValue);
+
+    return JSON.parseObject(jsonStr);
+  }
+
+  /**
+   * 把Java对象转换成JSON对象(自定义时间格式)
+   * 
+   * @param object
+   * @param dateFormate // 其他时间格式如: "MM月dd日 HH:mm"
+   * @return
+   */
+  public static JSONObject parseObjectToJSONObject(Object object, String dateFormate) {
+    String jsonStr = JSON.toJSONStringWithDateFormat(object, dateFormate);
 
     return JSON.parseObject(jsonStr);
   }
