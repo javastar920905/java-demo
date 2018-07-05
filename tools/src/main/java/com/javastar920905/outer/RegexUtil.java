eb277243d222360e334e117335638b66ed45b6f0
ouzhx <ouzhx@lt51.cn>
2018-07-05 10:41:41 +0800

    添加常用校验工具

--- tools/src/main/java/com/javastar920905/outer/RegexUtil.java
+++ tools/src/main/java/com/javastar920905/outer/RegexUtil.java
@@ -146,8 +146,42 @@ public class RegexUtil {
     return Pattern.compile(regex).matcher(input).matches();
   }
 
+  /**
+   * 验证是否是手机号
+   *
+   * @param phone
+   * @return
+   */
+  public static boolean verifyPhone(String phone) {
+    return matches("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$", phone);
+  }
 
-  /** 清除所有标签 **/
+  /**
+   * 验证是否是邮箱
+   *
+   * //p{Alpha}:内容是必选的，和字母字符[\p{Lower}\p{Upper}]等价。如：200896@163.com不是合法的。
+   *
+   * //w{2,15}: 2~15个[a-zA-Z_0-9]字符；w{}内容是必选的。 如：dyh@152.com是合法的。
+   *
+   * //[a-z0-9]{3,}：至少三个[a-z0-9]字符,[]内的是必选的；如：dyh200896@16.com是不合法的。
+   *
+   * //[.]:'.'号时必选的； 如：dyh200896@163com是不合法的。
+   *
+   * //p{Lower}{2,}小写字母，两个以上。如：dyh200896@163.c是不合法的。
+   *
+   * @param email
+   * @return
+   */
+  public static boolean verifyEmail(String email) {
+    return matches("\\p{Alpha}\\w{2,15}[@][a-z0-9]{3,}[.]\\p{Lower}{2,}", email);
+  }
+
+  /**
+   * 清除所有标签
+   *
+   * @param str 参数如: <a href="xx">跳转到</a>
+   * @return "跳转到" 去除了标签 常用组合: .replace("到","")
+   */
   public static String trimTag(String str) {
     return StringUtil.trim(str.replaceAll("<.+?>", ""));
   }
